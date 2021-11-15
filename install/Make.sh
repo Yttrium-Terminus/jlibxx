9229a72a70346abacd25bfbdc8d600dbbe5e5a4c
Jack Meng <jackmeng0814@gmail.com>
2021-11-15 13:40:51 -0500

    Update Make.sh

--- install/Make.sh
+++ install/Make.sh
@@ -1,2 +1,4 @@
 # A custom make file for this entire repository
-# This file runs when Install.sh calls this file to be run after the repository have been pulled
+# This file runs when Install.sh calls this file to be run after the repository has been pulled
+
+INSTALL_DIR="$HOME/JLibXX"
