c4e5cd2f3a0f064b9cd22bbcc0724743eaca2cff
Jack Meng <jackmeng0814@gmail.com>
2021-11-15 13:16:36 -0500

    Update Encode.java

--- jlibxx/util/Encode.java
+++ jlibxx/util/Encode.java
@@ -1,3 +1,20 @@
+/**
+ * <h1>Cryptography Encoding</h1>
+ * <p>jlibxx.util.Encode</p>
+ * 
+ * <p>
+ * This class is used specifically for the encryption process of cryptography. Most encryption methods will take
+ * a plaintext and attempt to encode it <strong>THIS CLASS DOES NOT ENCRYPT OR HASH ANY PLAINTEXT</strong>
+ * </p>
+ *
+ * For the reverse of this class please see @see jlibxx.util.Decode
+ * 
+ * @link https://en.wikipedia.org/wiki/Cipher
+ * @author Jack Meng
+ * @since 1.1
+ * @category cryptography
+ */
+
 package jlibxx.util;
 
 import jlibxx.human.en;
