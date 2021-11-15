40641adb7a6e1ae3cf810a789e92842adfd31862
Jack Meng <jackmeng0814@gmail.com>
2021-11-15 13:23:02 -0500

    Update Encode.java

--- jlibxx/util/Encode.java
+++ jlibxx/util/Encode.java
@@ -17,12 +17,35 @@
 
 package jlibxx.util;
 
+/**
+ * Avaliable packages used for the decryption of the plaintext from human language to language.
+ * @see jlibxx.human
+ * @since 1.0.1
+ * @code {new Encode(plaintext)}
+ * @code {new Decode(ciphertext)}
+ */
 import jlibxx.human.en;
 
 public class Encode {
+  /*
+   * Plaintext must be in standard ASCII font style, else this program will then attempt to convert it to unicode
+   * If the plaintext is given in ciphertext form, this class will further encrypt it "recursively"
+   * @link https://www.ws-attacks.org/Recursive_Cryptography
+   */
   private String s;
-  public Encode(String s) {
-    this.s = s;
+  
+  /**
+   * @param plaintext The plaintext that will be encrypted into the cipher
+   * @see jlibxx.util.Decode
+   * 
+   * <a>Avaliable Encoding Methods: 
+   * HEX, Base64, BaseX, UTF8, Standard-Unicode, Standard-ISO*, ASCII, JIS*, CNS*
+   * @see https://en.wikipedia.org/wiki/Character_encoding
+   * In courtesy of @link https://www.sciencedirect.com/topics/engineering/encoding-method
+   * </a>
+   */
+  public Encode(String plaintext) {
+    this.s = plaintext;
   }
 
   public boolean isEnglish() {
