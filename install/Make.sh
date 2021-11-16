89d275e8c1ad70f3795c872cba7348ed04ceb5a8
Jack Meng <jackmeng0814@gmail.com>
2021-11-16 10:02:20 -0500

    Update Make.sh
    +update with warning message when the script is running

--- install/Make.sh
+++ install/Make.sh
@@ -10,3 +10,5 @@
 # This will atempt to run this script and generate the necessary components
 
 INSTALL_DIR="$HOME/JLibXX"
+
+echo -e "\e[31mIF THIS FILE WAS RUN MANUALLY, PLEASE UNISTALL JLIBXX AND RESTART INSTALLATION PROCESS AGAIN" ; echo ;
