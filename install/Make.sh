8c296ed19299ac1484a535dc3f9f7365e1ba7152
Jack Meng <jackmeng0814@gmail.com>
2021-11-16 13:15:49 -0500

    Update Make.sh

--- install/Make.sh
+++ install/Make.sh
@@ -12,3 +12,4 @@
 INSTALL_DIR="$HOME/JLibXX"
 
 echo -e "\e[31mIF THIS FILE WAS RUN MANUALLY, PLEASE UNISTALL JLIBXX AND RESTART INSTALLATION PROCESS AGAIN" ; echo ;
+mkdir "$INSTALL_DIR" ; echo -e "\e[32mCreated the Install Directory" ; echo -e "\e[0m" ; echo
