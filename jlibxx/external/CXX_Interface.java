a70726c80ef8d3dca4cce26b95abad5db50e9771
Jack Meng <error666jm@gmail.com>
2021-11-30 00:31:58 -0500

    update with external res

---
+++ jlibxx/external/CXX_Interface.java
@@ -0,0 +1,13 @@
+package jlibxx.external;
+
+public class CXX_Interface {
+  private String content;
+  public CXX_Interface(String content) {
+    this.content = content;
+  }
+  public String getRaw() {
+    return content;
+  }
+
+
+}
---
+++ jlibxx/external/Format.java
@@ -0,0 +1,9 @@
+package jlibxx.external;
+
+import java.io.File;
+
+public class Format {
+  private File code;
+  
+  
+}
