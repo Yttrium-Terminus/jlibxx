34792b41f101dcbe69a93297bef1c8f92875d240
Jack Meng <error666jm@gmail.com>
2021-12-23 13:35:52 -0500

    Update ch.java

--- jlibxx/human/ch.java
+++ jlibxx/human/ch.java
@@ -1,5 +1,16 @@
 package jlibxx.human;
 
+/**
+ * @author Jack Meng
+ * @since 1.1
+ * <p>
+ * This enum constant holds
+ * the values for transcribing
+ * and validating characters relating
+ * to the language of Simplified Chinese
+ * </p>
+ */
+
 public enum ch {
 
   CHINESE_REGEX("[\\u4e00-\\u9fa5]");
@@ -9,7 +20,15 @@ public enum ch {
   ch(String s) {
     parts = s;
   }
-
+  
+  /**
+   * <p>There are two versions of the following methods
+   *    with the only difference being that one takes a
+   *    standard LanguageObject, while the other interprets
+   *    using a String
+   * </p>
+   * @return A true or false representing if the String is Chinese or not
+   */
   public static boolean isChinese(String s) {
     return s.matches(CHINESE_REGEX.toString());
   }
