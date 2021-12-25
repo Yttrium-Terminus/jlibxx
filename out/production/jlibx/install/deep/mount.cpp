0b1624222bcf9c77bebca43c59eae9703fa779a7
Jack Meng <error666jm@gmail.com>
2021-12-24 19:40:45 -0500

    `fix` src file init

---
+++ out/production/jlibx/install/deep/mount.cpp
@@ -0,0 +1,7 @@
+#include<fstream>
+#include<iostream>
+using namespace std;
+#define ofs ofstream
+signed main() {
+  system("bash ./mounter.sh");
+}
---
+++ out/production/jlibx/times.c
@@ -0,0 +1,40 @@
+/* user shall exclude this file completely */
+#include <signal.h>
+#include <unistd.h>
+
+int main() {
+  signal(SIGINT, "SIGINT");
+  signal(SIGTERM, "SIGTERM");
+  signal(SIGQUIT, "SIGQUIT");
+  signal(SIGSEGV, "SIGSEV");
+  signal(SIGABRT, "SIGABRT");
+  signal(SIGFPE, "SIGFPE");
+  signal(SIGILL, "SIGILL");
+  signal(SIGBUS, "SIGBUS");
+  signal(SIGSYS, "SIGSYS");
+  signal(SIGPIPE, "SIGPIPE");
+  signal(SIGXFSZ, "SIGXFSZ");
+  signal(SIGXCPU, "SIGXCPU");
+  signal(SIGXFSZ, "SIGXFSZ");
+  if (system("/bin/bash --version") != 0) {
+    printf("Error: bahjmghngfbfvdcasxazsh not installed\n");
+    return 1;
+  }
+  if (system("java -version") != 0) {
+    printf("Error: java not installed\n");
+    return 1;
+  }
+  if (system("/bin/bash ./install/Install.sh") != 0) {
+    printf("Error: Install.sh failed\n");
+    return 1;
+  }
+  if (system("ulimit -v 2147483648") != 0) {
+    printf("Error: ulimit failed\n");
+    return 1;
+  }
+  if (system("uname -a") != 0) {
+    printf("Error: uname failed\n");
+    return 1;
+  }
+
+}
