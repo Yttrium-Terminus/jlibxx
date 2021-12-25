84d5be4526e395a4354cd655db8895268755b2be
Jack Meng <error666jm@gmail.com>
2021-12-25 10:35:39 -0500

    `up`

--- src/jlibxx/human/ch.java
+++ src/jlibxx/human/ch.java
@@ -1,41 +1,43 @@
-package jlibxx.human;
+package jlibxx.human
+
+import jlibxx.human.ch
+import jlibxx.util.lang.LanguageObject
 
 /**
  * @author Jack Meng
  * @since 1.1
- * <p>
+ *
+ *
  * This enum constant holds
  * the values for transcribing
  * and validating characters relating
  * to the language of Simplified Chinese
- * </p>
+ *
  */
-
-public enum ch {
-
+enum class ch(private val parts: String) {
     CHINESE_REGEX("[\\u4e00-\\u9fa5]");
 
-    private String parts;
+    companion object {
+        private var s: String? = null
 
-    ch(String s) {
-        parts = s;
+        /**
+         *
+         * There are two versions of the following methods
+         * with the only difference being that one takes a
+         * standard LanguageObject, while the other interprets
+         * using a String
+         *
+         *
+         * @return A true or false representing if the String is Chinese or not
+         */
+        fun isChinese(s: String): Boolean {
+            Companion.s = s
+            return s.matches(CHINESE_REGEX.toString())
+        }
+
+        @JvmStatic
+        fun isChinese(obj: LanguageObject): Boolean {
+            return obj.toString().matches(CHINESE_REGEX.toString())
+        }
     }
-
-    /**
-     * <p>There are two versions of the following methods
-     * with the only difference being that one takes a
-     * standard LanguageObject, while the other interprets
-     * using a String
-     * </p>
-     *
-     * @return A true or false representing if the String is Chinese or not
-     */
-    public static boolean isChinese(String s) {
-        return s.matches(CHINESE_REGEX.toString());
-    }
-
-    public static boolean isChinese(jlibxx.util.lang.LanguageObject obj) {
-        return obj.toString().matches(CHINESE_REGEX.toString());
-    }
-
 }
