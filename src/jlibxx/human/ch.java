d2b148ffd54ac5a4a6ea0c113e4df44681a687d6
Jack Meng <error666jm@gmail.com>
2021-12-25 10:35:45 -0500

    `process` files that were old

--- src/jlibxx/human/ch.java
+++ src/jlibxx/human/ch.java
@@ -1,43 +1,43 @@
-package jlibxx.human
-
-import jlibxx.human.ch
-import jlibxx.util.lang.LanguageObject
+package jlibxx.human;
 
 /**
  * @author Jack Meng
  * @since 1.1
- *
- *
+ * <p>
  * This enum constant holds
  * the values for transcribing
  * and validating characters relating
  * to the language of Simplified Chinese
- *
+ * </p>
  */
-enum class ch(private val parts: String) {
+
+public enum ch {
+
     CHINESE_REGEX("[\\u4e00-\\u9fa5]");
 
-    companion object {
-        private var s: String? = null
+    private static String s;
+    private String parts;
 
-        /**
-         *
-         * There are two versions of the following methods
-         * with the only difference being that one takes a
-         * standard LanguageObject, while the other interprets
-         * using a String
-         *
-         *
-         * @return A true or false representing if the String is Chinese or not
-         */
-        fun isChinese(s: String): Boolean {
-            Companion.s = s
-            return s.matches(CHINESE_REGEX.toString())
-        }
-
-        @JvmStatic
-        fun isChinese(obj: LanguageObject): Boolean {
-            return obj.toString().matches(CHINESE_REGEX.toString())
-        }
+    ch(String s) {
+        parts = s;
     }
+
+    /**
+     * <p>There are two versions of the following methods
+     * with the only difference being that one takes a
+     * standard LanguageObject, while the other interprets
+     * using a String
+     * </p>
+     *
+     * @return A true or false representing if the String is Chinese or not
+     */
+    public static boolean isChinese(String s) {
+        ch.s = s;
+        return s.matches(CHINESE_REGEX.toString());
+    }
+
+    public static boolean isChinese(jlibxx.util.lang.LanguageObject obj) {
+        return obj.toString().matches(CHINESE_REGEX.toString());
+    }
+
 }
